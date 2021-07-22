7bdeb0327c873126136b0e46801d85bae3d00c4c
9ummy <godante27@gmail.com>
2021-07-23 03:44:08 +0900

    Implement async tag update function (not done)

--- appCodingNote/static/appCodingNote/js/viewRelated.js
+++ appCodingNote/static/appCodingNote/js/viewRelated.js
@@ -58,12 +58,27 @@ const hideNoteActionBtns = (noteId) => {
 
 const getTagNames = () => {
   const tagsSpanElements = document.getElementsByClassName('tag-span');
-  const tagsString = [];
+  const tagsArray = [];
   [...tagsSpanElements].forEach((span) => {
-    tagsString.push(span.innerHTML);
+    tagsArray.push(span.innerHTML);
   });
 
-  return tagsString.join(' ');
+  return tagsArray.join(' ');
+};
+
+const makeTagElements = (str) => {
+  const tagArray = str.split(' ');
+  const spanElements = [];
+  tagArray.forEach((string) => {
+    const newTag = document.createElement('span');
+    newTag.classList.add('tag-style');
+    newTag.classList.add('tag-span');
+    newTag.innerHTML = string;
+    spanElements.push(newTag);
+  });
+
+  console.log(spanElements);
+  return spanElements;
 };
 
 // 리스트뷰에서 노트 수정, 삭제
@@ -89,11 +104,12 @@ const editNote = (noteId, noteName, noteComment, noteLinkTitle) => {
 };
 
 const updateNote = async (folderId, noteId) => {
-  console.log('update 연결!');
-  let editNameElement = document.getElementById(`edit-name-${noteId}`);
-  let editCommentElement = document.getElementById(`edit-comment-${noteId}`);
-  let editLinkElement = document.getElementById(`edit-link-${noteId}`);
-  let editTagElement = document.getElementById(`edit-tag-${noteId}`);
+  const editNameElement = document.getElementById(`edit-name-${noteId}`);
+  const editCommentElement = document.getElementById(`edit-comment-${noteId}`);
+  const editLinkElement = document.getElementById(`edit-link-${noteId}`);
+  const editTagElement = document.getElementById(`edit-tag-${noteId}`);
+
+  const newTagElements = makeTagElements(editTagElement.value);
 
   let data = new FormData();
   data.append('noteName', editNameElement.value);
@@ -108,14 +124,17 @@ const updateNote = async (folderId, noteId) => {
     editCommentElement.value;
   document.getElementById(`note-link-${noteId}`).innerHTML =
     editLinkElement.value;
-  document.getElementById(`note-tag-${noteId}`).innerHTML =
-    editTagElement.value;
+
+  newTagElements.forEach((span) => {
+    document.getElementById(`note-tag-${noteId}`).appendChild(span);
+  });
 
   document.getElementById(`${noteId}-update-btn`).classList.add('no-display');
   document.getElementById(`${noteId}-edit-btn`).classList.remove('no-display');
   document
     .getElementById(`${noteId}-delete-btn`)
     .classList.remove('no-display');
+  document.getElementById(`edit-tag-${noteId}`).remove();
 };
 
 const deleteNote = async (folderId, noteId) => {
@@ -131,25 +150,37 @@ const deleteNote = async (folderId, noteId) => {
   }
 };
 
-const onClickUpdateButton = async (folderId, noteId) => {
-  console.log('update 연결!');
-  let editNameElement = document.getElementById(`edit-name-${noteId}`);
-  let editCommentElement = document.getElementById(`edit-comment-${noteId}`);
-  let editLinkElement = document.getElementById(`edit-link-${noteId}`);
-  let editTagElement = document.getElementById(`edit-tag-${noteId}`);
-  let data = new FormData();
-  data.append('noteName', editNameElement.value);
-  data.append('noteComment', editCommentElement.value);
-  data.append('noteLinkTitle', editLinkElement.value);
-  data.append('tag', editTagElement.value);
-  axios.post(`/codingnote/dashboard/${folderId}/${noteId}/updatenote/`, data);
-  let pastNameElement = document.getElementById(`note-name-${noteId}`);
-  pastNameElement.innerHTML = editNameElement.value;
-  let pastCommentElement = document.getElementById(`note-comment-${noteId}`);
-  pastCommentElement.innerHTML = editCommentElement.value;
-  let pastLinkElemenT = document.getElementById(`note-link-${noteId}`);
-  pastLinkElemenT.innerHTML = editLinkElement.value;
-  let pastTagElement = document.getElementById(`note-tag-${noteId}`);
-  pastTagElement.innerHTML = editTagElement.value;
-  document.getElementById(`${noteId}-update-btn`).classList.add('hide');
+// TODO : 아래 수정하기
+const addNote = (folderId) => {
+  const noteElement = document.getElementById('table-body');
+  let newTabletr = document.createElement('tr');
+  let newTableName = document.createElement('td');
+  newTableName.setAttribute('id', 'new-name-row');
+  newTableName.innerHTML = `<input id="table-name-${folderId}" type="text", placeholder="(필수)제목을 입력해주세요.", name="name"></input>`;
+  let newTableComment = document.createElement('td');
+  newTableComment.setAttribute('id', 'new-comment-row');
+  newTableComment.innerHTML = `<input id="table-comment-${folderId}" type="text", placeholder="(선택)코멘트를 입력해주세요.", name="comment"></input>`;
+  let newTableWebsite = document.createElement('td');
+  newTableWebsite.setAttribute('id', 'new-website-row');
+  newTableWebsite.innerHTML = `<input id="table-website-${folderId}" type="text", placeholder="(필수)url을 입력해주세요.", name="link-title"></input>`;
+  let newTableTag = document.createElement('td');
+  newTableTag.setAttribute('id', 'new-tag-row');
+  newTableTag.innerHTML = `<input id="table-tag-${folderId}" type="text", placeholder="(선택)태그를 입력해주세요.", name="tag"></input>`;
+  let newTableAction = document.createElement('td');
+  let newTableSaveButton = document.createElement('button');
+  newTableSaveButton.setAttribute('class', 'save');
+  newTableSaveButton.setAttribute('id', `${folderId}-save-btn`);
+  newTableSaveButton.setAttribute('onclick', `onClickSaveButton(${folderId})`);
+  newTableSaveButton.innerHTML =
+    '<img class="save-img" src="/static/img/save-update.svg" />';
+
+  newTableAction.append(newTableSaveButton);
+  noteElement.appendChild(newTabletr);
+  newTabletr.append(
+    newTableName,
+    newTableComment,
+    newTableWebsite,
+    newTableTag,
+    newTableAction
+  );
 };
